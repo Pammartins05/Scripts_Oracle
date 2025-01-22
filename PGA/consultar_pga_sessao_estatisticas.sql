48176aa4caae491fc2ca3c177ca26041b7a54c18
Pamela Martins <pammartinsn27@gmail.com>
2025-01-22 06:36:30 -0300

    Initial commit

---
+++ PGA/consultar_pga_sessao_estatisticas.sql
@@ -0,0 +1,15 @@
+-- consulta estatisticas de consumo de memoria PGA por sessao
+SELECT    nvl(username,';
+') as username, 
+          COUNT(1) ;
+,
+          round(sum(value/1024/1024),2) mbytes
+FROM      v$statname n, 
+          v$session s, 
+          v$sesstat t
+WHERE     s.sid=t.sid
+AND       n.statistic#=t.statistic#
+AND       s.type='USER'
+AND       s.username IS NOT NULL
+AND       n.name = 'session pga memory'
+GROUP BY  ROLLUP (username)
